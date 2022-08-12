18213cfa440bdce61ef7858c710caed02881a6fd
unknown <shahidulislam7150@gmail.com>
2022-08-12 20:58:52 +0600

    navbar resposive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { Container } from "reactstrap";
 import logo from "../../assets/images/res-logo.png";
@@ -22,6 +22,9 @@ const nav_links = [
   },
 ];
 const Header = () => {
+
+    const menuRef=useRef(null);
+    const toggleMenu=()=>menuRef.current.classList.toggle('show_menu')
   return (
     <header className="header">
       <Container>
@@ -32,10 +35,10 @@ const Header = () => {
           </div>
 
           {/* ------------menu---------- */}
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <div className="menu d-flex align-items-center gap-5">
               {nav_links.map((item, index) => (
-                <NavLink to={item.path} key={index}
+                <NavLink  to={item.path} key={index}
                 className={navClass=>navClass.isActive?'active_menu'
             :''
             }
@@ -48,16 +51,16 @@ const Header = () => {
           {/* nav with icon  */}
           <div className="nav_right d-flex align-items-center gap-4">
             <span className="cart_icon">
-              <i class="ri-shopping-basket-line"></i>
+            <i class="ri-shopping-basket-line"></i>
               
               <span className="cart_badge">2</span>
             </span>
             <span className="user">
               <Link to="/login">
-                <i class="ri-user-line"></i>
+              <i class="ri-user-line"></i>
               </Link>
             </span>
-            <span className="mobile_menu">
+            <span className="mobile_menu" onClick={toggleMenu}>
               <i class="ri-menu-line"></i>
             </span>
           </div>
